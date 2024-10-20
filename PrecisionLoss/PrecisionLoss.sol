ba60bcfd587ce09dadcb738ac84a1b477378b6f7
DonkoTonev <donkotonev04@gmail.com>
2024-10-20 16:37:08 +0300

    Quick change

--- PrecisionLoss/PrecisionLoss.sol
+++ PrecisionLoss/PrecisionLoss.sol
@@ -17,17 +17,4 @@ contract PrecisionTest {
         }
     }
 
-    function _mintShares(address _recipient, uint256 _amount) public {
-        require(_recipient != address(0), "Mint to the zero address");
-
-        if (totalShares == 0) {
-            shares[address(0)] = DEAD_SHARES;
-            totalShares = DEAD_SHARES;
-            _amount -= DEAD_SHARES;
-        }
-
-        totalShares += _amount;
-        shares[_recipient] += _amount;
-    }
-
 }
